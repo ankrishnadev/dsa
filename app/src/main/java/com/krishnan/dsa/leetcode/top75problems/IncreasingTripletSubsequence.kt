880422fc0fc6f194e585650fb99b5e85bda8c70f
ankrishnadev <ankrishnadev@gmail.com>
2024-07-18 01:10:24 +0530

    IncreasingTripletSubsequence

--- app/src/main/java/com/krishnan/dsa/leetcode/top75problems/IncreasingTripletSubsequence.kt
+++ app/src/main/java/com/krishnan/dsa/leetcode/top75problems/IncreasingTripletSubsequence.kt
@@ -22,3 +22,21 @@ fun increasingTriplet(nums: IntArray): Boolean {
     }
     return false
 }
+
+//Best approach
+fun increasingTriplet1(nums: IntArray): Boolean {
+    var min = Int.MAX_VALUE
+    var max = Int.MAX_VALUE
+
+    nums.forEach {
+        if (it <= min) {
+            min = it
+        } else if (it <= max) {
+            max = it
+        } else {
+            return true
+        }
+    }
+
+    return false
+}
