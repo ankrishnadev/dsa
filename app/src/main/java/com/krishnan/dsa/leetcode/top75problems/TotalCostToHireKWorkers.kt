e682a3b7e7c9f979d6e3e99e8d99e20f5cfcb1c3
ankrishnadev <ankrishnadev@gmail.com>
2024-10-04 23:36:34 +0530

    TotalCostToHireKWorkers

---
+++ app/src/main/java/com/krishnan/dsa/leetcode/top75problems/TotalCostToHireKWorkers.kt
@@ -0,0 +1,46 @@
+package com.krishnan.dsa.leetcode.top75problems
+
+import java.util.PriorityQueue
+
+fun main() {
+    val costArray = intArrayOf(17, 12, 10, 2, 7, 2, 11, 20, 8)
+    val k = 3
+    val candidates = 4
+    println(totalCost(costArray, k, candidates))
+}
+
+fun totalCost(costs: IntArray, k: Int, candidates: Int): Long {
+    var total: Long = 0
+
+    var i = candidates
+    var j = maxOf(i, costs.size - candidates)
+
+    val queue = PriorityQueue<Int>()
+
+    fun left(c: Int) = queue.add(costs[c] * 2)
+    fun right(c: Int) = queue.add(costs[c] * 2 + 1)
+    fun addValue(n: Int) {
+        total += n / 2
+    }
+
+    fun isLeft(n: Int): Boolean = n % 2 == 0
+
+    for (c in 0 until k) {
+        left(c)
+    }
+
+    for (c in costs.indices) {
+        right(c)
+    }
+
+    repeat(k) {
+        val v = queue.remove()
+        addValue(v)
+        if (i < j) {
+            if (isLeft(v)) left(i++) else right(--j)
+        }
+    }
+
+    return total
+
+}
