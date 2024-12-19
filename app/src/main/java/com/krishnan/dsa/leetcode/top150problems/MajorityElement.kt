fa95e6fa05729e51b91e37935f4d60f5f0b23111
ankrishnadev <ankrishnadev@gmail.com>
2024-12-19 23:45:24 +0530

    MajorityElement

---
+++ app/src/main/java/com/krishnan/dsa/leetcode/top150problems/MajorityElement.kt
@@ -0,0 +1,18 @@
+package com.krishnan.dsa.leetcode.top150problems
+
+fun main() {
+    val array = intArrayOf(2, 2, 1, 1, 1, 2, 2)
+    println(majorityElement(array))
+}
+
+fun majorityElement(array: IntArray): Int {
+    var count = 0
+    var previousValue: Int = 0
+    for (num in array) {
+        if (count == 0) {
+            previousValue = num
+        }
+        count += if (num == previousValue) 1 else -1
+    }
+    return count
+}
