14f9ff800b393885a4a13580de9a6e2778ffc8bf
Krishnan Singaram <ankrishnadev@gmail.com>
2025-01-09 23:56:07 +0530

    H-Index - Counting sort approach

--- app/src/main/java/com/krishnan/dsa/leetcode/top150problems/HIndex.kt
+++ app/src/main/java/com/krishnan/dsa/leetcode/top150problems/HIndex.kt
@@ -3,6 +3,7 @@ package com.krishnan.dsa.leetcode.top150problems
 fun main() {
     val citation = intArrayOf(3, 0, 6, 1, 5)
     println(hIndex(citation))
+    println(hIndex1(citation))
 }
 
 //Sort and Linear scan approach
@@ -16,4 +17,28 @@ fun hIndex(citation: IntArray): Int {
     }
 
     return h
+}
+
+//Counting sort approach (Optimized for large datasets)
+fun hIndex1(citation: IntArray): Int {
+    val n = citation.size
+    val count = IntArray(n + 1)
+
+    for (value in citation) {
+        if (value >= n){
+            count[n]++
+        } else {
+            count[value]++
+        }
+    }
+
+    var total = 0
+    for (i in n downTo 0) {
+        total += count[i]
+        if (total >= i) {
+            return i
+        }
+    }
+
+    return 0
 }
