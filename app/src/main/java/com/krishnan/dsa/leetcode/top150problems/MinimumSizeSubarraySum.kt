107ac2b9ff81c1178c8aafddf4dbe7905f158b40
Krishnan <ankrishnadev@gmail.com>
2025-03-29 19:51:07 +0530

    MinimumSizeSubarraySum modification.

--- app/src/main/java/com/krishnan/dsa/leetcode/top150problems/MinimumSizeSubarraySum.kt
+++ app/src/main/java/com/krishnan/dsa/leetcode/top150problems/MinimumSizeSubarraySum.kt
@@ -1,25 +1,24 @@
 package com.krishnan.dsa.leetcode.top150problems
 
-import kotlin.math.min
-
 fun main() {
-    val input = intArrayOf(2, 3, 1, 2, 4, 3)
-    println(minSubArrayLen(7, input))
+    val input = intArrayOf(1, 2, 3, 4, 5)
+    println(minSubArrayLen(11, input))
 }
 
 fun minSubArrayLen(target: Int, nums: IntArray): Int {
 
     var left = 0
     var total = 0
-    var result = Int.MAX_VALUE
+    var answer = Int.MAX_VALUE
 
-    for (i in nums.indices) {
-        total += nums[i]
-        while (total >= target){
-            result = min(result, i + 1 - left)
+    for (right in nums.indices) {
+        total += nums[right]
+
+        while (total >= target) {
+            answer = minOf(answer, right - left + 1)
             total -= nums[left++]
         }
     }
 
-    return if(result == Int.MAX_VALUE) 0 else result
+    return if (answer == Int.MAX_VALUE) 0 else answer
 }
